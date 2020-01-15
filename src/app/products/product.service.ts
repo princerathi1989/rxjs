9513a4bf7ab31c47cb4348f306dc02446218fdcf
Prince Chaudhary <princerathi1989@gmail.com>
2020-01-16 01:06:28 +0530

    Revert "Map returned data - price raise by 50%"
    This reverts commit 644ec74c73f845da83be73da2354109fcddf4764.

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap, map } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 
@@ -13,13 +13,6 @@ export class ProductService {
   private productsUrl = 'api/products';
   products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
-      map(products =>
-        products.map(product => ({
-          ...product,
-          price: product.price * 1.5,
-          searchKey: [product.productName]
-        }))
-      ),
       tap(data => console.log('Products: ', JSON.stringify(data))),
       catchError(this.handleError)
     );
