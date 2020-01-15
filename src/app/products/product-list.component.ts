859f3cb892c1fd4c345d78c51d086ac4ed40d604
Prince Chaudhary <princerathi1989@gmail.com>
2020-01-16 01:06:53 +0530

    Revert "Async pipe and code clear"
    This reverts commit 876935bf40e3a73b23cb8affa798a84315521e09.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import {  Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -9,17 +9,26 @@ import { ProductService } from './product.service';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products$: Observable<Product[]>;
+  products: Product[] = [];
+  sub: Subscription;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products$ = this.productService.getProducts();
+    this.sub = this.productService.getProducts()
+      .subscribe(
+        products => this.products = products,
+        error => this.errorMessage = error
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
   onSelected(categoryId: string): void {
