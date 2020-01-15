c3fbc3a92c2f4e36ba8175dc39850c75e42da908
Prince Chaudhary <princerathi1989@gmail.com>
2020-01-15 13:37:00 +0530

    catchError

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,9 +1,11 @@
+import { catchError } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 
-import {  Observable } from 'rxjs';
+import { Observable, of, EMPTY } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -19,7 +21,13 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products$ = this.productService.getProducts();
+    this.products$ = this.productService.getProducts()
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
   }
 
   onSelected(categoryId: string): void {
